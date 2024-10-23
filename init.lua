335875d8de7d9ba5208ab1b2314385d72f71615d
george <george@uddersorchard.co.uk>
2024-10-23 11:10:42 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+-- vim configurations
+vim.g.mapleader= " "
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2") 
+vim.cmd("set number")
+
+-- plugins
+require("config.lazy")
+
+
+-- keyMaps
+
